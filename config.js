e8666ce538a016829f56cee1056a09bb2e60d2c5
Ken Kotch <hello@ken.uno>
2017-11-17 20:47:16 -0700

    update package and config

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 export const facebook = {
   clientID: '355116831617012',
   clientSecret: 'dcc6fe2b9766a604c08d8b1f3f03ff7a',
-  callbackURL: 'https://my-bike.herokuapp.com/auth/facebook/callback',
+  callbackURL: 'http://localhost:3000/auth/facebook/callback',
   profileFields: ['id', 'name', 'displayName', 'picture', 'email'],
 };
 
 export const google = {
   clientID: '212800111227-5alhcstm2hitugdm3t5qmv6iesk14fi4.apps.googleusercontent.com',
   clientSecret: '9pNmncupVQA26SA1ncpWPu3K',
-  callbackURL: 'com.googleusercontent.apps.212800111227-tppiu630f0opsad3k6m5s32e9ajos312',
+  callbackURL: 'http://localhost:3000/auth/google',
 };
