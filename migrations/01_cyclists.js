85f1a6b6bfadf770c03c62f41257435b9c8c1f71
Ken Kotch <hello@ken.uno>
2017-11-17 20:22:43 -0700

    prep for heroku

---
+++ migrations/01_cyclists.js
@@ -0,0 +1,11 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('cyclists', (table)=>{
+    table.increments('id').primary()
+    table.varchar('email').notNullable().unique()
+    table.varchar('token')
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('cyclists')
+};
---
+++ migrations/03_components.js
@@ -0,0 +1,13 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('components', (table)=>{
+    table.increments('id').primary();
+    table.integer('chain').notNullable()
+    table.integer('tires').notNullable()
+    table.integer('brake_pads').notNullable()
+    table.integer('bike_id').references('id').inTable('bikes').onDelete('cascade')
+  })
+}
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('components')
+}
